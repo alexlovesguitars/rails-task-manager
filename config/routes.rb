f8f99a203e8021166f3cd68f89a5bda4eda02922
Alexander Pfitzner <alexander.pfitzner@gmx.net>
2025-02-09 09:43:44 +0100

    Task Manager

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,24 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+  get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks/:id/", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  delete "restaurants/:id", to: "restaurants#destroy"
 end
+
+# READ ALL
+# get "tasks", to: "tasks#index"
+# CREATE (Step 1 creates new entry/form, Step 2 creates entry from form)
+# get "tasks/new", to: "tasks#new", as: :new_task
+# post "tasks", to: "tasks#create"
+# READ ONE
+# get "tasks/:id", to: "tasks#show", as: :task
+# EDIT - (Step 1 gets entry to edit, Step 2 saves changes)
+# get "tasks/:id/", to: "tasks#edit", as: :edit_task
+# patch "tasks/:id", to: "tasks#update"
+# DELETE
+# delete "restaurants/:id", to: "restaurants#destroy"
